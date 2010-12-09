58669ed15ca6a857baee8d0b64541610beeec547
David R. Bild <drbild@umich.edu>
2010-12-09 00:24:44 +0000

    Corrected build issues in Android.mk

--- user/Android.mk
+++ user/Android.mk
@@ -1,9 +1,11 @@
+ifneq ($(TARGET_SIMULATOR),true)
+
 LOCAL_PATH:=$(call my-dir)
 
-include $CLEAR_VARS)
-LOCAL_SRC_FILES:=masond.c
-LOCAL_MODULE:=masond
-
-LOCAL_SHARED_LIBRARIES := libc
-
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= masond.c
+LOCAL_MODULE:= masond
+LOCAL_SHARED_LIBRARIES := libc libcutils
 include $(BUILD_EXECUTABLE)
+
+endif
