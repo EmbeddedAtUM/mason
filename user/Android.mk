f52ec0ba182f588f46cbf960a5052bdfd3d0fd5c
David R. Bild <drbild@umich.edu>
2011-02-04 14:32:30 -0500

    Add masond includes directory to Android build

--- user/Android.mk
+++ user/Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= masond.c
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/../kernel/include/
 LOCAL_MODULE:= masond
 LOCAL_SHARED_LIBRARIES := libc libcutils
 include $(BUILD_EXECUTABLE)
